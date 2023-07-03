f492a8ebe4cca03ece1b411e57b00bf485e4d71b
Tom Ames <tsames1@gmail.com>
2023-07-03 09:26:27 -0700

    Basic test for accessing Spoonacular API via Django.

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import get_recipe
 
 urlpatterns = [
-    path('recipes/<int:id>/', get_recipe)
+    path('recipes/', get_recipe)
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+
+API_KEY = ''
 
 # Create your views here.
 def get_recipe(request, id=None):
-    message = f'You have retrieved ID {id}.'
-    return HttpResponse(message)
+    url=f'https://api.spoonacular.com/recipes/findByIngredients?ingredients=onions&apiKey={API_KEY}&number=1&limitLicense=true&ranking=1&ignorePantry=false&sortDirection=asc&addRecipeInformation=true&instructionsRequired=true&maxReadyTime=5'
+    response = requests.get(url)
+    data = response.json()
+    return HttpResponse(data)
