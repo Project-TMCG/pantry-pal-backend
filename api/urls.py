775b0e41da4801d3c10f4a6f199125730bd38d9e
Tom Ames <tsames1@gmail.com>
2023-07-05 16:42:32 -0700

    Working on including query data for spoonaclular api via the request body.

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import get_recipe
 
 urlpatterns = [
-    path('recipes/<slug:ingredients>/', get_recipe),
-    path('recipes/<slug:ingredients>/<str:diet>', get_recipe)
+    path('recipes/', get_recipe)
 ]
--- api/views.py
+++ api/views.py
@@ -7,12 +7,20 @@ API_KEY = ''
 if settings.DEBUG:
     API_KEY=settings.API_KEY
 
+def getRequestBody(body):
+    body_unicode = body.decode('utf-8')
+    body_data = json.loads(body_unicode)
+
 def assembleUrl(queryParams):
     url=f'https://api.spoonacular.com/recipes/complexSearch?apiKey={API_KEY}'
 
 # Create your views here.
-def get_recipe(request, ingredients):
-    
+def get_recipe(request,):
+    body_unicode = request.body.decode('utf-8')
+    body_data = json.loads(body_unicode)
+
+    print(request.body)
+    url=f'https://api.spoonacular.com/recipes/complexSearch?apiKey={API_KEY}'
     response = requests.get(url)
     data = response.json()
     return JsonResponse(data)
