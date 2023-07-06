a13b9b6cf4273fd65ba3d6d9f492e1d1c9776c96
Tom Ames <tsames1@gmail.com>
2023-07-05 17:22:44 -0700

    Spoonacular api route now acepts json body data to customize query options.

--- api/views.py
+++ api/views.py
@@ -1,26 +1,40 @@
 from django.conf import settings
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 import requests
+import json
+
+
+# %$%$%$%$%$%$% Environ Variable %$%$%$%$%$%$%
 
 API_KEY = ''
 
 if settings.DEBUG:
     API_KEY=settings.API_KEY
 
-def getRequestBody(body):
-    body_unicode = body.decode('utf-8')
-    body_data = json.loads(body_unicode)
+# %$%$%$%$%$%$% Helper Functions %$%$%$%$%$%$%
 
-def assembleUrl(queryParams):
-    url=f'https://api.spoonacular.com/recipes/complexSearch?apiKey={API_KEY}'
-
-# Create your views here.
-def get_recipe(request,):
+def parseBody(request):
     body_unicode = request.body.decode('utf-8')
     body_data = json.loads(body_unicode)
+    return body_data
 
-    print(request.body)
+def constructQueryUrl(body_data):
     url=f'https://api.spoonacular.com/recipes/complexSearch?apiKey={API_KEY}'
+
+    if (body_data["ingredients"]):
+        i_list = ','.join(body_data["ingredients"])
+        url = url + f'&includeIngredients={i_list}'
+
+    return url
+
+# %$%$%$%$%$%$% Views %$%$%$%$%$%$%
+# Create your views here.
+def get_recipe(request):
+    body_data = parseBody(request)
+    url = constructQueryUrl(body_data)
+
+    # return HttpResponse("Testing route without accessing Spoonacular API")
+    print(url)
     response = requests.get(url)
     data = response.json()
     return JsonResponse(data)
