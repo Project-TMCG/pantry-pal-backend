d749678175c065467c07c8513ad94272ed6c9125
Tom Ames <tsames1@gmail.com>
2023-07-07 09:32:41 -0700

    The Django Bakend Route that send request(s) to the Spoonacular API now accepts a get request to /api/recipes/ with the query parameters entered in the Request Body - the query parameters must be entered exactly as they are in the Spooncaular docs for complex search.

--- api/views.py
+++ api/views.py
@@ -14,27 +14,35 @@ if settings.DEBUG:
 # %$%$%$%$%$%$% Helper Functions %$%$%$%$%$%$%
 
 def parseBody(request):
+
     body_unicode = request.body.decode('utf-8')
     body_data = json.loads(body_unicode)
     return body_data
 
 def constructQueryUrl(body_data):
-    url=f'https://api.spoonacular.com/recipes/complexSearch?apiKey={API_KEY}'
 
-    if (body_data["ingredients"]):
-        i_list = ','.join(body_data["ingredients"])
-        url = url + f'&includeIngredients={i_list}'
+    url=f'https://api.spoonacular.com/recipes/complexSearch?apiKey={API_KEY}&instructionsRequired=true'
+
+    if("number" in body_data):
+        url = url + f'&number={body_data["number"]}'
+    else:
+        url = url + '&number=10'
+
+    for key in body_data:
+        if(key != "number"):
+            url = url + f'&{key}={body_data[key]}'
 
     return url
 
 # %$%$%$%$%$%$% Views %$%$%$%$%$%$%
-# Create your views here.
+
 def get_recipe(request):
+
+    #Parse Body
     body_data = parseBody(request)
     url = constructQueryUrl(body_data)
 
-    # return HttpResponse("Testing route without accessing Spoonacular API")
-    print(url)
+    #Search for Recipes that Match the Query Parameters
     response = requests.get(url)
     data = response.json()
     return JsonResponse(data)
